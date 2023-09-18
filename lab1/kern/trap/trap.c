27c75ac32d52e3af6e653e90194e24963b95b270
shockstove <2247157972@qq.com>
2023-09-18 14:02:30 +0800

    修改：     lab1/kern/trap/trap.c

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -164,7 +164,7 @@ void exception_handler(struct trapframe *tf) {
             */
             cprintf("Exception type:Illegal instruction\n");
             cprintf("Illegal instruction caught at 0x%p\n",tf->epc);
-            
+            tf->epc+=2;
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
@@ -173,6 +173,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type:breakpoint\n");
+            cprintf("breakpoint caught at 0x%p\n",tf->epc);
+            tf->epc+=2;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
