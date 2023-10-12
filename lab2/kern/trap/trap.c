3adc40d6d74777172cba569ed02f39c311e1cd94
jtysxtm <1193156762@qq.com>
2023-10-13 01:29:01 +0800

    lab2

--- lab2/kern/trap/trap.c
+++ lab2/kern/trap/trap.c
@@ -8,10 +8,8 @@
 #include <riscv.h>
 #include <stdio.h>
 #include <trap.h>
-#include <sbi.h>
 
 #define TICK_NUM 100
-volatile size_t num=0;
 
 static void print_ticks() {
     cprintf("%d ticks\n", TICK_NUM);
@@ -128,16 +126,8 @@ void interrupt_handler(struct trapframe *tf) {
             // cprintf("Supervisor timer interrupt\n");
             // clear_csr(sip, SIP_STIP);
             clock_set_next_event();
-            ticks++;
-            if(ticks==TICK_NUM)
-            {
+            if (++ticks % TICK_NUM == 0) {
                 print_ticks();
-                ticks=0;
-                num++;
-            }
-            if(num==10)
-            {
-                sbi_shutdown();
             }
             break;
         case IRQ_H_TIMER:
@@ -171,14 +161,8 @@ void exception_handler(struct trapframe *tf) {
         case CAUSE_FAULT_FETCH:
             break;
         case CAUSE_ILLEGAL_INSTRUCTION:
-            cprintf("Exception type:Illegal instruction\n");
-            cprintf("Illegal instruction caught at 0x%p\n",tf->epc);
-            tf->epc+=4;
             break;
         case CAUSE_BREAKPOINT:
-            cprintf("Exception type:breakpoint\n");
-            cprintf("breakpoint caught at 0x%p\n",tf->epc);
-            tf->epc+=2;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
