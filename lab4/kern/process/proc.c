5dd253203ef290cb911ee19aab473080f0937e3e
shockstove <2247157972@qq.com>
2023-11-18 17:38:28 +0800

    modified:   lab4/kern/process/proc.c

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -102,7 +102,18 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-
+        proc->state = PROC_UNINIT;
+        proc->pid = -1;
+        proc->runs = 0;
+        proc->kstack = 0;
+        proc->need_resched = 0;
+        proc->parent = NULL;
+        proc->mm = NULL;
+        memset(&(proc->context), 0, sizeof(struct context));
+        proc->tf = NULL;
+        proc->cr3 = boot_cr3;
+        proc->flags = 0;
+        memset(proc->name, 0, PROC_NAME_LEN+1);
 
     }
     return proc;
